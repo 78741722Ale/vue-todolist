8396061cccc790b2c8c0a035c14c88eab47972e2
78741722Ale <pecorillaalessandro@gmail.com>
2022-04-22 14:02:20 +0200

    Aggiunto logo (dinamico) e classi dinamiche

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,9 @@ const app = new Vue({
     el: '#app',
     // dati contenuti
     data: {
-
+        logo: './assets/img/logo.png',// Logo Boolean
+        f_container: 'container-md mt-3', // Primo container
+        f_row: 'row h_sm', // Prima row
+        logo_col: 'col-12 d-flex justify-content-center align-items-center' // Colonna del logo
     },
 })
