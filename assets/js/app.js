d4190afbacdc44ff186eb29f3816db2845e6773e
78741722Ale <pecorillaalessandro@gmail.com>
2022-04-22 14:27:29 +0200

    Aggiunto stile ai button, nuove classi dinamiche e refactoring al foglio di stile

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,11 @@ const app = new Vue({
     // dati contenuti
     data: {
         logo: './assets/img/logo.png',// Logo Boolean
-        f_container: 'container-md mt-3', // Primo container
-        f_row: 'row h_sm', // Prima row
-        logo_col: 'col-12 d-flex justify-content-center align-items-center' // Colonna del logo
+        f_container: 'container-md mt-3', // Primo container (logo)
+        s_container: 'container-sm mt-3',
+        f_row: 'row h_sm', // Prima row (logo)
+        s_row: 'row h_xs d-flex gx-3 justify-content-center align-items-center', // Seconda row (Input e Button)
+        input_class: 'w-100 p-2 text_sm radius',
+        logo_col: 'col-12 d-flex justify-content-center align-items-center' // prima col (logo)
     },
 })
