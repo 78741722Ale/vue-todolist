f188b5bd1bab5af6272d219b7e3c4b4bca1822d7
78741722Ale <pecorillaalessandro@gmail.com>
2022-04-22 14:58:50 +0200

    Aggiunta 3 section per le task, nuove classi dinamiche aggiunte a data

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,18 @@ const app = new Vue({
     // dati contenuti
     data: {
         logo: './assets/img/logo.png',// Logo Boolean
+        // Containers
         f_container: 'container-md mt-3', // Primo container (logo)
         s_container: 'container-sm mt-3',
+        t_container: 'container-sm mt-3',
+        // Rows
         f_row: 'row h_sm', // Prima row (logo)
         s_row: 'row h_xs d-flex gx-3 justify-content-center align-items-center', // Seconda row (Input e Button)
-        input_class: 'w-100 p-2 text_sm radius',
-        logo_col: 'col-12 d-flex justify-content-center align-items-center' // prima col (logo)
+        t_row: 'row p-3 justify-content-center align-items-center bg_t_cont flex-wrap',
+        // Classi Utilities 
+        input_class: 'w-100 p-2 text_sm radius', // Classi in comune tra button e Type Text
+        logo_col: 'col-12 d-flex justify-content-center align-items-center', // prima col (logo)
+        cross_button: 'col-1 d-flex justify-content-end align-items-center border-0',
+        task_class: 'col-4 d-flex justify-content-start align-items-center',
     },
 })
