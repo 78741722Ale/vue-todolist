e277894198ad34db1cade8b1b16e17719566778b
78741722Ale <pecorillaalessandro@gmail.com>
2022-04-23 14:40:35 +0200

    Creati nuovi componenti della DOM e nuove classi dinamiche

--- versione-alternativa/assets/js/app_2.js
+++ versione-alternativa/assets/js/app_2.js
@@ -5,13 +5,15 @@ const app = new Vue({
     data: {
         // Classi dinamiche
         container_class: "container p-0", // Main container
-        row_class: "row g-0 gy-3 p-0 d-flex justify-content-center align-items-center flex-wrap", // Row per le col
-        logo_input: "col-5 bg_prova h-25 d-flex justify-content-center flex-wrap", // Colonna per logo e input
-        main_tasks: "col-12 bg_prova h-75", // Colonna per 3 mini container
+        row_class: "row g-0 p-0 d-flex justify-content-center align-items-center flex-wrap", // Row per le col
+        logo_input: "col-5 h-25 d-flex justify-content-center flex-wrap", // Colonna per logo e input
+        main_tasks: "col-12 h-75 d-flex justify-content-evenly align-items-center", // Colonna per 3 mini container
         // Tutte classi del logo
         col_logo: "col-8 h-50 d-flex justify-content-center",
         logo: "../assets/img/logo.png", // immagine del logo
-        logo_class: "w-100 h-100" // classi del logo
+        logo_class: "w-100 h-100", // classi del logo
+        // Tutte le classi dell'input
+        col_input: "col-12 d-flex justify-content-center align-items-start",
     },
 
 
